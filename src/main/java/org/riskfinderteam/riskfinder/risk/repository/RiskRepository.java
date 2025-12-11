127a8fd910714d15f8b5f7ec7b8d7f879a973535
Tarae0419 <tarae0419@gmail.com>
2025-12-12 01:07:02 +0900

    feat: 레포 수정

--- src/main/java/org/riskfinderteam/riskfinder/risk/repository/RiskRepository.java
+++ src/main/java/org/riskfinderteam/riskfinder/risk/repository/RiskRepository.java
@@ -1,13 +1,19 @@
 package org.riskfinderteam.riskfinder.risk.repository;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import org.riskfinderteam.riskfinder.risk.dto.CustomerFactorDto;
 import org.riskfinderteam.riskfinder.risk.dto.CustomerMailDto;
 import org.riskfinderteam.riskfinder.risk.entity.ScoringResult;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RiskRepository extends JpaRepository<ScoringResult, Long> {
     CustomerFactorDto getTop3FeaturesBySkIdCurr(Long skIdCurr);
-    CustomerMailDto getCustomerMailData(Long skIdCurr);
+
+    @Query("SELECT new org.riskfinderteam.riskfinder.risk.dto.CustomerMailDto(s.grade, s.top3Features) " +
+            "FROM ScoringResult s " +
+            "WHERE s.skIdCurr = :customerId")
+    CustomerMailDto getCustomerMailData(@Param("customerId") Long customerId);
 }
